4a1bfae044737030dea2757c1a430d5f5b45cee7
alexfalco <afalconer02@gmail.com>
2020-01-09 12:45:33 -0500

    LighthouseBnB Reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -50,7 +50,7 @@ const getUserWithId = function(id) {
   FROM users
   WHERE id = $1
   `, [id])
-  .then(res => res.rows) 
+  .then(res => res.rows[0]) 
 }
 exports.getUserWithId = getUserWithId;
 
@@ -80,8 +80,21 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function(guest_id) {
+  return pool.query(`
+  SELECT d.id,d.title,d.cost_per_night,c.start_date,d.rating
+  FROM reservations as c
+  JOIN (
+  SELECT b.id as id,title,cost_per_night, avg(a.rating) as rating
+  FROM property_reviews as a
+  JOIN properties as b on b.id = a.property_id
+  GROUP BY b.id,title,cost_per_night
+  ) as d on d.id = c.property_id
+  WHERE c.guest_id = $1 and start_date < now()
+  ORDER BY start_date
+  LIMIT 10
+  `, [guest_id])
+  .then(res => res.rows) 
 }
 exports.getAllReservations = getAllReservations;
 
